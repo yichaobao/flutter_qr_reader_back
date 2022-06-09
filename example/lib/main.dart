92f8fefa7d065fe0caeee58d0168ca2bef287b85
Emmanel <yichaobao@163.com>
2022-06-09 15:18:17 +0800

    备份

---
+++ example/lib/main.dart
@@ -0,0 +1,137 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_qr_reader/flutter_qr_reader.dart';
+import 'package:flutter_qr_reader_example/scanViewDemo.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  QrReaderViewController? _controller;
+  bool isOk = false;
+  String? data;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+              onPressed: () async {
+                PermissionStatus permissions =
+                    await Permission.camera.request();
+                print(permissions);
+                if (permissions == PermissionStatus.granted) {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        child: Text("ok"),
+                      );
+                    },
+                  );
+                  setState(() {
+                    isOk = true;
+                  });
+                }
+              },
+              child: Text("请求权限"),
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () async {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ScanViewDemo()));
+              },
+              child: Text("独立UI"),
+            ),
+            FlatButton(
+                onPressed: () async {
+                  var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+                  if (image == null) return;
+                  final rest = await FlutterQrReader.imgScan(File(image.path));
+                  setState(() {
+                    data = rest;
+                  });
+                },
+                child: Text("识别图片")),
+            FlatButton(
+                onPressed: () {
+                  assert(_controller != null);
+                  _controller?.setFlashlight();
+                },
+                child: Text("切换闪光灯")),
+            FlatButton(
+                onPressed: () {
+                  assert(_controller != null);
+                  _controller?.startCamera(onScan);
+                },
+                child: Text("开始扫码（暂停后）")),
+            if (data != null) Text(data??""),
+            if (isOk)
+              Container(
+                width: 320,
+                height: 350,
+                child: QrReaderView(
+                  width: 320,
+                  height: 350,
+                  callback: (container) {
+                    this._controller = container;
+                    _controller?.startCamera(onScan);
+                  },
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onScan(String v, List<Offset> offsets) {
+    print([v, offsets]);
+    setState(() {
+      data = v;
+    });
+    _controller?.stopCamera();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
